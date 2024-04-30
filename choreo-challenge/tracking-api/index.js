41ecaf6643aba11651eddb7b2bd7318b9011e440
jairo-fernandezr <jairo.fernandez@gmail.com>
2024-04-29 23:36:37 -0400

    revision 5

--- choreo-challenge/tracking-api/index.js
+++ choreo-challenge/tracking-api/index.js
@@ -31,7 +31,7 @@ app.get('/track/:trackingNumber', (req, res) => {
 });
 
 // Specify the port to listen on
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
