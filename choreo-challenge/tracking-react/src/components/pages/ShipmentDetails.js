27f32707a621051d9dcd6578faaea10394849bcc
jairo-fernandezr <jairo.fernandez@gmail.com>
2024-04-30 20:27:07 -0400

    revision 16

--- choreo-challenge/tracking-react/src/components/pages/ShipmentDetails.js
+++ choreo-challenge/tracking-react/src/components/pages/ShipmentDetails.js
@@ -10,7 +10,15 @@ import useFetch from '../hooks/useFetch';
 
 //const apiUrl = window?.configs?.apiUrl ? window.configs.apiUrl : "http://localhost:8080";
 
-const apiUrl = 'https://be858289-8f36-4453-baf1-92ce88cf27ce-dev.e1-us-cdp-2.choreoapis.dev/test-tracking-system/test-tracking-api/tracking-api-5c6/v1.0';
+//const apiUrl = 'https://be858289-8f36-4453-baf1-92ce88cf27ce-dev.e1-us-cdp-2.choreoapis.dev/test-tracking-system/test-tracking-api/tracking-api-5c6/v1.0';
+
+// const apiUrl = 'http://localhost:8080';
+
+const apiUrl = '/choreo-apis/test-tracking-system/test-tracking-api/tracking-api-5c6/v1';
+
+
+
+
 
 const ShipmentDetails = () => {
   const { track_num } = useParams();
